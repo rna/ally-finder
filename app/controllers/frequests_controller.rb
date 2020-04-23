a95c645dcaca3389febc1c0979a21ab7af1e1483
Ramesh <rameshnaidu.allu@gmail.com>
2020-04-23 17:40:30 +0530

    fix linter error

--- app/controllers/frequests_controller.rb
+++ app/controllers/frequests_controller.rb
@@ -16,8 +16,8 @@ class FrequestsController < ApplicationController
     @user = params[:friend]
     friend_request = current_user.inverse_frequests.find_by_user_id(@user)
     friend_request.status = true
-    friend_request_2 = current_user.frequests.new(friend_id: @user, status: true)
-    if friend_request.save && friend_request_2.save
+    new_friend_row = current_user.frequests.new(friend_id: @user, status: true)
+    if friend_request.save && new_friend_row.save
       flash[:notice] = 'Friend request accepted'
     else
       flash[:error] = 'Error in accepting Friend request'
