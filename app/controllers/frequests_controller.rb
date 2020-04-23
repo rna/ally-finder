125d4ef897129748f7502adef6d197f2922b2c61
Ramesh <rameshnaidu.allu@gmail.com>
2020-04-23 17:37:08 +0530

    add 2 rows for mutual friendship in the model

--- app/controllers/frequests_controller.rb
+++ app/controllers/frequests_controller.rb
@@ -16,7 +16,8 @@ class FrequestsController < ApplicationController
     @user = params[:friend]
     friend_request = current_user.inverse_frequests.find_by_user_id(@user)
     friend_request.status = true
-    if friend_request.save
+    friend_request_2 = current_user.frequests.new(friend_id: @user, status: true)
+    if friend_request.save && friend_request_2.save
       flash[:notice] = 'Friend request accepted'
     else
       flash[:error] = 'Error in accepting Friend request'
